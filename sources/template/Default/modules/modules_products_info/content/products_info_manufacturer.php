4b7361ffead67e4378592d28c034f24978710711
ClicShopping <Yae8mr2o*>
2020-01-18 15:40:58 -0500

    different update json, synthax

--- sources/template/Default/modules/modules_products_info/content/products_info_manufacturer.php
+++ sources/template/Default/modules/modules_products_info/content/products_info_manufacturer.php
@@ -16,7 +16,7 @@ use ClicShopping\OM\CLICSHOPPING;
   <div style="float: <?php echo MODULE_PRODUCTS_INFO_MANUFACTURER_POSITION; ?>;">
     <div class="moduleProductsInfoInfoManufacturer">
       <span class="moduleProductsInfoInfoManufacturer"><h3><?php echo CLICSHOPPING::getDef('module_products_info_manufacturer_title'); ?> : </h3></span>
-      <span itemprop="manufacturer" itemscope itemtype="https://schema.org/Organization" class="ProductsInfoManufacturer"><meta itemprop="name" content="<?php echo $manufacturer; ?>" /><h3><?php echo $manufacturer; ?></h3></span>
+      <span class="ProductsInfoManufacturer"><h3><?php echo $manufacturer; ?></h3></span>
     </div>
   </div>
 </div>
